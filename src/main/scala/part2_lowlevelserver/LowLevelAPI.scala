593ed4073a23acdd86a028b7b8a1654d54311638
metherton <martinetherton63@gmail.com>
2021-02-19 23:09:49 +0100

    Low level API - 2

--- src/main/scala/part2_lowlevelserver/LowLevelAPI.scala
+++ src/main/scala/part2_lowlevelserver/LowLevelAPI.scala
@@ -3,6 +3,7 @@ package part2_lowlevelserver
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.Http.IncomingConnection
+import akka.http.scaladsl.model.headers.Location
 import akka.http.scaladsl.model.{ContentTypes, HttpEntity, HttpMethods, HttpRequest, HttpResponse, StatusCodes, Uri}
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.{Flow, Sink}
@@ -52,6 +53,8 @@ object LowLevelAPI extends App {
           """.stripMargin
         )
       )
+
+
 
     case request: HttpRequest =>
       request.discardEntityBytes()
@@ -177,4 +180,75 @@ object LowLevelAPI extends App {
 
   // shorthand
   Http().bindAndHandle(streamsBasedRequestHandler, "localhost", 8082)
+
+
+  /*
+      Exercise: create your own HTTP server running on localhost on 8388 , which replies
+      - with a welcome message on the front door localhost:8388
+      - with a proper HTML on localhost:8388/about
+      - with a 404 message otherwise
+   */
+
+  val exerciseBasedRequestHandler: Flow[HttpRequest, HttpResponse, _] = Flow[HttpRequest].map {
+
+    case HttpRequest(HttpMethods.GET, Uri.Path("/about"), _, _, _) => // method, URI, HTTP headers, content and the protocol (HTTP1.1/HTTP2.0)
+      HttpResponse(
+        StatusCodes.OK, // http 200
+        entity = HttpEntity(
+          ContentTypes.`text/html(UTF-8)`,
+          """
+            |<html>
+            |<body>
+            |about me
+            |</body>
+            |</html>
+          """.stripMargin
+        )
+      )
+
+    // path /search redirects to some other part of our app / web / microservice
+    case HttpRequest(HttpMethods.GET, Uri.Path("/search"), _, _, _) =>
+      HttpResponse(
+        StatusCodes.Found, // http 302
+        headers = List(Location("http://google.com"))
+      )
+
+    case HttpRequest(HttpMethods.GET, Uri.Path("/"), _, _, _) => // method, URI, HTTP headers, content and the protocol (HTTP1.1/HTTP2.0)
+      HttpResponse(
+        StatusCodes.OK, // http 200
+        entity = HttpEntity(
+          ContentTypes.`text/html(UTF-8)`,
+          """
+            |<html>
+            |<body>
+            |welcome to the front door
+            |</body>
+            |</html>
+          """.stripMargin
+        )
+      )
+
+    case request: HttpRequest =>
+      request.discardEntityBytes()
+      HttpResponse(
+        StatusCodes.NotFound, // 404
+        entity = HttpEntity(
+          ContentTypes.`text/html(UTF-8)`,
+          """
+            |<html>
+            |<body>
+            |404 - OOPS the resource can't be found
+            |</body>
+            |</html>
+          """.stripMargin
+
+        )
+      )
+  }
+
+  val bindingFuture = Http().bindAndHandle(exerciseBasedRequestHandler, "localhost", 8388)
+  bindingFuture.flatMap(binding => binding.unbind())
+    .onComplete(_ => system.terminate())
+
+
 }
