7a7798765f5907d2ef344b9ad38600f4718cdf7c
metherton <martinetherton63@gmail.com>
2021-02-23 21:58:51 +0100

    HighLevelExample first pass

---
+++ src/main/scala/part3_highlevelserver/HighLevelExample.scala
@@ -0,0 +1,108 @@
+package part3_highlevelserver
+
+import akka.actor.{ActorSystem, Props}
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
+import akka.stream.ActorMaterializer
+import part2_lowlevelserver.GuitarDB.CreateGuitar
+import part2_lowlevelserver.LowLevelRest.system
+import part2_lowlevelserver.{Guitar, GuitarDB, GuitarStoreJsonProtocol}
+import akka.pattern.ask
+import akka.util.Timeout
+
+import scala.concurrent.Future
+import scala.concurrent.duration._
+
+// step 1
+import spray.json._
+
+object HighLevelExample extends App with GuitarStoreJsonProtocol {
+
+
+  implicit val system = ActorSystem("HighLevelExample")
+  implicit val materializer = ActorMaterializer()
+
+  import system.dispatcher
+  import akka.http.scaladsl.server.Directives._
+
+  import GuitarDB._
+
+  /*
+      GET /api/guitar fetches ALL the guitars in the store
+      GET /api/guitar?id=x fetches the guitar with id x
+      GET /api/guitar/x fetches guitar with id x
+      GET /api/guitar/inventory?inStock=true
+
+   */
+
+  /*
+
+    set up
+ */
+  val guitarDb = system.actorOf(Props[GuitarDB], "LowLevelGuitarDB")
+  val guitarList = List(
+    Guitar("Fender", "Stratocaster"),
+    Guitar("Gibson", "Les Paul"),
+    Guitar("Martin", "LX1")
+  )
+  guitarList.foreach { guitar =>
+    guitarDb ! CreateGuitar(guitar)
+  }
+
+  implicit val timeout = Timeout(2 seconds)
+
+  val guitarServerRoute =
+    path("api" / "guitar") {
+      // ALWAYS PUt THE MORE SPECIFIC ROUTE FIRSt
+      parameter('id.as[Int]) { (guitarId: Int) =>
+        get {
+          val guitarFuture: Future[Option[Guitar]] = (guitarDb ? FindGuitar(guitarId)).mapTo[Option[Guitar]]
+          val entityFuture = guitarFuture.map { guitarOption =>
+            HttpEntity(
+              ContentTypes.`application/json`,
+              guitarOption.toJson.prettyPrint
+            )
+          }
+          complete(entityFuture)
+        }
+      } ~
+      get {
+        val guitarFuture: Future[List[Guitar]] = (guitarDb ? FindAllGuitars).mapTo[List[Guitar]]
+        val entityFuture = guitarFuture.map { guitars =>
+          HttpEntity(
+            ContentTypes.`application/json`,
+            guitars.toJson.prettyPrint
+          )
+        }
+        complete(entityFuture)
+      }
+    } ~
+    path("api" / "guitar" / IntNumber) { guitarId =>
+      get {
+        val guitarFuture: Future[Option[Guitar]] = (guitarDb ? FindGuitar(guitarId)).mapTo[Option[Guitar]]
+        val entityFuture = guitarFuture.map { guitarOption =>
+          HttpEntity(
+            ContentTypes.`application/json`,
+            guitarOption.toJson.prettyPrint
+          )
+        }
+        complete(entityFuture)
+      }
+    } ~
+    path("api" / "guitar" / "inventory") {
+      get {
+        parameter('inStock.as[Boolean]) { inStock =>
+          val guitarFuture: Future[List[Guitar]] = (guitarDb ? FindGuitarsInStock(inStock)).mapTo[List[Guitar]]
+          val entityFuture = guitarFuture.map { guitars =>
+            HttpEntity(
+              ContentTypes.`application/json`,
+              guitars.toJson.prettyPrint
+            )
+          }
+          complete(entityFuture)
+        }
+      }
+    }
+  Http().bindAndHandle(guitarServerRoute, "localhost", 8080)
+
+}
